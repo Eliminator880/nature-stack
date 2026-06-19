e4ca39f67df461bdcb289f178e8cd336bdbe0ba8
Chris Goodin <cgoodin@cavs.msstate.edu>
2026-06-19 10:18:40 -0500

    Added a launch file for obstacle field test

---
+++ launch/obsfield_batch_launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the array of numbers
+speeds=(5 7 9 11)
+#speeds=(11)
+num_iter=50
+
+# Loop through each number in the array
+for num in "${speeds[@]}"; do
+    # Loop from 1 to num_iter for iterations
+    for ((i=1; i<=num_iter; i++)); do
+    #for ((i=28; i<=num_iter; i++)); do
+        filename="mavs_obsfield_rosbag_speed_${num}_test_${i}"
+        echo $filename
+        ros2 launch mavs_obsfield_experiment.launch.py vehicle_speed:=${num}.0 bag_name:="$filename"
+
+    done
+done
